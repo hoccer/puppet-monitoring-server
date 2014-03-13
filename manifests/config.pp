96596bd7d45055015141670150ad281ed4afa376
Andreas Schröder <kaazoo@artcom.de>
2014-03-13 17:07:23 +0100

    fix permissions add log directory

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,8 +3,15 @@ class monitoring-server::config {
   file { '/etc/riemann.config':
     owner  => root,
     group  => root,
-    mode   => 440,
+    mode   => 644,
     source => 'puppet:///modules/monitoring-server/riemann/riemann.config',
+  }
+
+  file { '/var/log/riemann':
+    ensure => directory,
+    owner  => riemann,
+    group  => riemann,
+    mode   => 755,
   }
 
 }
