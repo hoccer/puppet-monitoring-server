730a0df708a85884b52da9cb221b2e09fef1ece2
Andreas Schröder <kaazoo@artcom.de>
2014-03-13 17:59:58 +0100

    set RVM Ruby version

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,9 @@
 class monitoring-server::install {
 
   class { 'riemann':
-    config_file => '/etc/riemann.config',
-    version     => '0.2.4'
+    config_file     => '/etc/riemann.config',
+    version         => '0.2.4',
+    rvm_ruby_string => 'ruby-2.0.0-p353'
   }
 
   include riemann
