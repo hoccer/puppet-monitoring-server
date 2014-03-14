aa52eeaf73727c5b64e2d6bdc9bac08061e6b8a5
Andreas Schröder <kaazoo@artcom.de>
2014-03-14 11:43:31 +0100

    add layout configuration for riemann-dash specify rvm_ruby_string

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,4 +14,20 @@ class monitoring-server::config {
     mode   => 755,
   }
 
+  file { '/var/lib/riemann-dash':
+    ensure => directory,
+    owner  => riemann-dash,
+    group  => riemann-dash,
+    mode   => 755,
+  }
+
+  file { '/var/lib/riemann-dash/layout.json':
+    owner  => root,
+    group  => root,
+    mode   => 644,
+    source => 'puppet:///modules/monitoring-server/riemann-dash/layout.json',
+    require => File['/var/lib/riemann-dash']
+  }
+
+
 }
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,15 @@
 class monitoring-server::install {
 
   class { 'riemann':
-    config_file     => '/etc/riemann.config',
-    version         => '0.2.4',
+    config_file => '/etc/riemann.config',
+    version     => '0.2.4',
+  }
+
+  class { 'riemann::dash':
+    rvm_ruby_string => 'ruby-2.0.0-p353'
+  }
+
+  class { 'riemann::tools':
     rvm_ruby_string => 'ruby-2.0.0-p353'
   }
 
