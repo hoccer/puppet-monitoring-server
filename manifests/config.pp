ab62ddc6a86e8872c699cd35cad40615c8587c30
Andreas Schröder <kaazoo@artcom.de>
2014-03-17 19:24:22 +0100

    add config for Nagios

--- manifests/config.pp
+++ manifests/config.pp
@@ -30,19 +30,67 @@ class monitoring-server::config {
   }
 
 
-  nginx::resource::upstream { 'nagios':
+  nginx::resource::upstream { 'fcgiwrap':
     members => [
-      'localhost:8443',
+      'unix:/var/run/fcgiwrap.socket',
+    ],
+  }
+
+  nginx::resource::upstream { 'php':
+    members => [
+      'unix:/var/run/php5-fpm.socket',
     ],
   }
 
   nginx::resource::vhost { 'nagios.hoccer.de':
-    proxy => 'http://nagios',
     listen_port          => 443,
     ssl                  => true,
     ssl_cert             => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key              => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_port             => 443,
+    auth_basic           => 'Nagios',
+    auth_basic_user_file => '/etc/nagios3/htpasswd.users',
+    index_files          => ['index.php', 'index.html'],
+    www_root             => '/usr/share/nagios3/htdocs',
+    use_default_location => false,
+  }
+
+  nginx::resource::location { 'stylesheets_loc':
+    location       => '/stylesheets',
+    vhost          => 'nagios.hoccer.de',
+    location_alias => '/etc/nagios3/stylesheets',
+    ssl            => true,
+    ssl_only       => true,
+  }
+
+  nginx::resource::location { 'cgi_loc':
+    location                   => '~ \.cgi$',
+    vhost                      => 'nagios.hoccer.de',
+    www_root                   => '/usr/lib/cgi-bin/nagios3',
+    fastcgi                    => 'fcgiwrap',
+    fastcgi_script             => '/usr/lib/cgi-bin/nagios3$fastcgi_script_name',
+    ssl                        => true,
+    ssl_only                   => true,
+    location_custom_cfg_append => [
+      'fastcgi_param AUTH_USER $remote_user;',
+      'fastcgi_param REMOTE_USER $remote_user;',
+      'rewrite ^/cgi-bin/nagios3/(.*)$ /$1;',
+    ],
+  }
+
+  nginx::resource::location { 'php_loc':
+    location                   => '~ \.php$',
+    vhost                      => 'nagios.hoccer.de',
+    fastcgi                    => 'php',
+    fastcgi_script             => '$document_root$fastcgi_script_name',
+    ssl                        => true,
+    ssl_only                   => true,
+    try_files                  => [
+      '$uri =404',
+    ],
+    location_custom_cfg_append => [
+      'fastcgi_index index.php;',
+    ],
   }
 
   nginx::resource::upstream { 'ntop':
@@ -58,6 +106,8 @@ class monitoring-server::config {
     ssl_cert             => '/etc/ssl/certs/ssl-cert-snakeoil.pem',
     ssl_key              => '/etc/ssl/private/ssl-cert-snakeoil.key',
     ssl_port             => 443,
+    auth_basic           => 'NTOP',
+    auth_basic_user_file => '/etc/nagios3/htpasswd.users',
   }
 
   nginx::resource::upstream { 'riemann-dash':
